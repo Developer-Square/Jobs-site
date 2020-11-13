9699be11eefff9f81b9d8b64afd84a5f3157a5e6
Adrian <athenb@localhost.localdomain>
2020-11-13 11:30:36 +0300

    🚧 restructured the landing page

--- src/pages/LandingPage/comments.js
+++ src/pages/LandingPage/comments.js
@@ -1,39 +1,31 @@
-import ericImage from "image/eric.jpg";
-import markImage from "image/mark.jpg";
-import adrianImage from "image/adrian.jpg";
-import larryImage from "image/lawrence.jpeg";
+import mcImage from "image/mastercard.jpg";
+import nImage from "image/nation.jpg";
+import mkImage from "image/mckinsey.jpg";
+
 
 export const comments = [
   {
     id: 1,
-    name: "Mark Thumi",
-    imageUrl: markImage,
+    name: "Mastercard",
+    imageUrl: mcImage,
     comment:
-      "The leaders of tomorrow are here. It's about time the youth came together to address youth-related challenges starting with the complex issue of unemployment.",
-    position: "CEO and Student at Africa Nazarene",
+      "Kenya’s gig economy is nascent, buoyant and continues to grow with more than 60% of gig workers entering the ecosystem between 2017 and 2019",
+    position: "Payments Provider",
   },
   {
     id: 2,
-    name: "Mang'are Adrian",
-    imageUrl: adrianImage,
+    name: "Nation",
+    imageUrl: nImage,
     comment:
-      "'I praise You, Father, Lord of heaven and earth, because You have hidden these things from the wise and learned, and revealed them to little children.' - Jesus",
-    position: "CTO and Stratizen",
+      "Over 35 per cent of respondents said that finding gig work was easier on a digital platform, and about 30 percent said platforms made faster payments possible, and helped them connect to other workers.",
+    position: "News Agency",
   },
   {
     id: 3,
-    name: "Lawrence Kamau",
-    imageUrl: larryImage,
+    name: "McKinsey",
+    imageUrl: mkImage,
     comment:
-      "Lawrence is a talented engineer. He sees an infinity of opportunities in technology and uses it to build impressive solutions.",
-    position: "VP Engineering",
-  },
-  {
-    id: 4,
-    name: "Eric Ochieng'",
-    imageUrl: ericImage,
-    comment:
-      "God-fearing, creative and an innovative individual. Driven to be the change the youth aspire, a voice in youth unemployment and youth created jobs.",
-    position: "CFO and Stratizen",
+      "People who actively chose their working style reported greater satisfaction than those who felt forced by circumstance.",
+    position: "Management Consulting"
   },
 ];
