7f8629118ad83aeeb9866f7bd451209713729053
Ryan Njoroge <keddelyronjoz@gmail.com>
2021-07-29 07:12:47 +0300

    fix: Testing on the pagination issue

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -15,4 +15,4 @@ export const BASE_GRAPHQL_URL =
 export const CURRENCY = "Ksh.";
 export const serviceWorkerTimeout =
   parseInt(process.env.SERVICE_WORKER_TIMEOUT, 10) || 60 * 1000;
-export const vacancyLimit = 2;
+export const vacancyLimit = 3;
