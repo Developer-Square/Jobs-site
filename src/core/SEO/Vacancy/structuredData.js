ed595f8dbe9f54352d0c49782532021ac0fe8a25
larrykamau <lawrencekamau5@gmail.com>
2021-10-26 09:30:51 +0300

    seo job fixtures

--- src/core/SEO/Vacancy/structuredData.js
+++ src/core/SEO/Vacancy/structuredData.js
@@ -1,17 +1,81 @@
-export const structuredData = (vacancy) => {
-  // const images = vacancy.images.map((image) => new URL(image.url).pathname);
-  // const { variants } = vacancy;
+import urljoin from "url-join";
 
+import { paths } from "@paths";
+
+export const structuredData = (shop = {}) => {
+  return JSON.stringify({
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    description: shop
+      ? shop.description
+      : "From Gigs to Internships to Jobs and Volunteering. students can gerrit, employers can get it, workers can gerrit. EVERYBODY can gerrit! TheDatabase, Jobs Need People!",
+    name: shop ? shop.name : "The Database Kenya",
+    potentialAction: {
+      "@type": "SearchAction",
+      "query-input": "required name=q",
+      target: urljoin(window.location.href, paths.search, "?q={q}"),
+    },
+    url: window.location.href,
+  });
+};
+
+// export const structuredData = (vacancy) => {
+//   // const images = vacancy.images.map((image) => new URL(image.url).pathname);
+//   // const { variants } = vacancy;
+
+//   return JSON.stringify({
+//     "@context": "https://schema.org/",
+//     "@type": "Internship",
+//     // "@type": "Vacancy",
+//     description: !vacancy.seoDescription
+//       ? `${vacancy.descriptionPlaintext}`
+//       : `${vacancy.seoDescription}`,
+//     image: vacancy.creator.avatar.url,
+//     name: !vacancy.seoTitle ? `${vacancy.name}` : `${vacancy.seoTitle}`,
+//     // offers: getVariantsStructuredData(variants),
+//     url: window.location.href,
+//   });
+// };
+
+export const jobStructuredData = (vacancy) => {
   return JSON.stringify({
     "@context": "https://schema.org/",
-    "@type": "Internship",
-    // "@type": "Vacancy",
-    description: !vacancy.seoDescription
-      ? `${vacancy.description_plaintext}`
-      : `${vacancy.seoDescription}`,
-    image: vacancy.creator.avatar.url,
-    name: !vacancy.seoTitle ? `${vacancy.name}` : `${vacancy.seoTitle}`,
-    // offers: getVariantsStructuredData(variants),
-    url: window.location.href,
+    "@type": "JobPosting",
+    title: vacancy?.title,
+    description: vacancy?.descriptionPlaintext,
+    identifier: {
+      "@type": "PropertyValue",
+      name: "Google",
+      value: "1234567",
+    },
+    datePosted: vacancy?.createdAt,
+    validThrough: vacancy?.closingDate,
+    employmentType: vacancy?.jobType,
+    hiringOrganization: {
+      "@type": "Organization",
+      name: vacancy?.postedBy?.name,
+      sameAs: vacancy?.postedBy?.website,
+      logo: vacancy?.postedBy?.logo?.url,
+    },
+    jobLocation: {
+      "@type": "Place",
+      address: {
+        "@type": "PostalAddress",
+        streetAddress: "Nairobi, Kenya",
+        addressLocality: "Nairobi, Kenya",
+        addressRegion: "NBI",
+        postalCode: "00200",
+        addressCountry: "KE",
+      },
+    },
+    baseSalary: {
+      "@type": "MonetaryAmount",
+      currency: vacancy?.currency,
+      value: {
+        "@type": "QuantitativeValue",
+        value: vacancy?.salary,
+        unitText: vacancy?.payRate,
+      },
+    },
   });
 };
--- src/pages/Profile/SeekerForm.js
+++ src/pages/Profile/SeekerForm.js
@@ -22,66 +22,64 @@ const SeekerForm = ({
       {(formik) => {
         return (
           <Form>
-            <h4>Profile Details</h4>
-            <div className="submit-page">
-              <div className="form grid grid-cols-2 gap-6">
-                <FormikControl
-                  control="input"
-                  type="text"
-                  label="Title"
-                  placeholder="e.g. Student, Eng, Mr etc"
-                  name="title"
-                />
-                <FormikControl
-                  control="input"
-                  type="number"
-                  label="ID Number"
-                  placeholder="ID Number"
-                  name="idNumber"
-                />
-              </div>
-              <div className="form grid grid-cols-2 gap-6">
-                <FormikControl
-                  control="select"
-                  label="Status"
-                  name="status"
-                  hideButton={() => {}}
-                  style={{ margin: 0 }}
-                  options={statusOptions}
-                  defaultValue={{
-                    value: "",
-                    label: "Select Options",
-                  }}
-                />
-                <FormikControl
-                  control="select"
-                  label="Gender"
-                  name="gender"
-                  hideButton={() => {}}
-                  style={{ margin: 0 }}
-                  options={genderOptions}
-                  defaultValue={{ value: "", label: "Select Gender" }}
-                />
-              </div>
-              <div className="form">
-                <FormikControl
-                  control="phone"
-                  type="phone"
-                  label="Phone Number"
-                  placeholder="e.g. +254 722-123123"
-                  name="mobile"
-                />
-              </div>
-              <div className="form">
-                <FormikControl
-                  control="input"
-                  type="text"
-                  label="Current Residence (County, Place)"
-                  name="location"
-                  placeholder="e.g. Nairobi, Kasarani - Corner"
-                />
-              </div>
-              {/* <div className="form">
+            <div className="form grid grid-cols-2 gap-6">
+              <FormikControl
+                control="input"
+                type="text"
+                label="Title"
+                placeholder="e.g. Student, Eng, Mr etc"
+                name="title"
+              />
+              <FormikControl
+                control="input"
+                type="number"
+                label="ID Number"
+                placeholder="ID Number"
+                name="idNumber"
+              />
+            </div>
+            <div className="form grid grid-cols-2 gap-6">
+              <FormikControl
+                control="select"
+                label="Status"
+                name="status"
+                hideButton={() => {}}
+                style={{ margin: 0 }}
+                options={statusOptions}
+                defaultValue={{
+                  value: "",
+                  label: "Select Options",
+                }}
+              />
+              <FormikControl
+                control="select"
+                label="Gender"
+                name="gender"
+                hideButton={() => {}}
+                style={{ margin: 0 }}
+                options={genderOptions}
+                defaultValue={{ value: "", label: "Select Gender" }}
+              />
+            </div>
+            <div className="form">
+              <FormikControl
+                control="phone"
+                type="phone"
+                label="Phone Number"
+                placeholder="e.g. +254 722-123123"
+                name="mobile"
+              />
+            </div>
+            <div className="form">
+              <FormikControl
+                control="input"
+                type="text"
+                label="Current Residence (County, Place)"
+                name="location"
+                placeholder="e.g. Nairobi, Kasarani - Corner"
+              />
+            </div>
+            {/* <div className="form">
                           <FormikControl
                             control="date"
                             label="Birth Date"
@@ -89,36 +87,35 @@ const SeekerForm = ({
                             maxDate={new Date(2003, 1, 1)}
                           />
                         </div> */}
-              <div className="form" style={{ width: "100%" }}>
-                <FormikControl
-                  control="select"
-                  label="Interests"
-                  name="industries"
-                  hideButton={() => {}}
-                  style={{ margin: 0 }}
-                  options={industries}
-                  isMulti={true}
-                />
-              </div>
-              <div className="form" style={{ width: "100%" }}>
-                <FormikControl
-                  control="textarea"
-                  label="Additional Info"
-                  name="description"
-                  rte={true}
-                  fullWidth
-                />
-              </div>
-              <div className="form" style={{ width: "100%" }}>
-                <Button
-                  type="submit"
-                  disabled={!formik.isValid}
-                  fullwidth
-                  loading={loading}
-                  title={loading ? "Saving... " : "Save"}
-                  className="button margin-top-15"
-                />
-              </div>
+            <div className="form" style={{ width: "100%" }}>
+              <FormikControl
+                control="select"
+                label="Interests"
+                name="industries"
+                hideButton={() => {}}
+                style={{ margin: 0 }}
+                options={industries}
+                isMulti={true}
+              />
+            </div>
+            <div className="form" style={{ width: "100%" }}>
+              <FormikControl
+                control="textarea"
+                label="Additional Info"
+                name="description"
+                rte={true}
+                fullWidth
+              />
+            </div>
+            <div className="form" style={{ width: "100%" }}>
+              <Button
+                type="submit"
+                disabled={!formik.isValid}
+                fullwidth
+                loading={loading}
+                title={loading ? "Saving... " : "Save"}
+                className="button margin-top-15"
+              />
             </div>
           </Form>
         );
